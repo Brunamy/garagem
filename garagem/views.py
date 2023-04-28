679596d44c185cf6fcc9d9ad21fd7f937d9e5a5a
brunamy <bm6952@gmail.com>
2023-04-28 15:00:21 -0300

    Criação Serializer e API Marca

--- garagem/views.py
+++ garagem/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from garagem.models import Marca
+from garagem.serializers import MarcaSerializer
+# class AcessorioVieSet(ModelViewSet):
+#     queryset = Acessorio.objects.all()
+#     serializer_class = AcessorioSerializer
+
+# class CategoriaVieSet(ModelViewSet):
+#     queryset = Categoria.objects.all()
+#     serializer_class = CategoriaSerializer
+
+# class CorVieSet(ModelViewSet):
+#     queryset = Cor.objects.all()
+#     serializer_class = CorSerializer
+
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+# class VeiculoViewSet(ModelViewSet):
+#     queryset = Veiculo.objects.all()
+#     # serializer_class = VeiculoSerializer
+
+#     def get_serializer_class(self):
+#         if self.action in ["list", "retrieve"]:
+#             return VeiculoDetailSerializer
+#         return VeiculoSerializer
